c79887b377bdc7b5117253584e33256e499a968c
Ketu Patel <pketu916@gmail.com>
2025-06-11 13:56:52 +0530

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "./App.css";
 import Header from "./component/header";
 import { Hero } from "./component/hero";
@@ -8,17 +7,18 @@ import SkillGrid from "./component/skills";
 
 function App() {
 
-  useEffect(() => {
-  const setVH = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
+//   useEffect(() => {
+//   const setVH = () => {
+//     const vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+//   };
 
-  setVH();
+//   setVH();
 
-  window.addEventListener('resize', setVH);
-  return () => window.removeEventListener('resize', setVH);
-}, []);
+//   window.addEventListener('resize', setVH);
+//   return () => window.removeEventListener('resize', setVH);
+// }, []);
+
 
   return (
     <>
