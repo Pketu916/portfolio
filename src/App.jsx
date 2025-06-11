2de5c9ba9386627b988606f76be845aa049f7652
Ketu Patel <pketu916@gmail.com>
2025-06-11 17:10:24 +0530

    remove marqee

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,11 @@
 import "./App.css";
 import Header from "./component/header";
 import { Hero } from "./component/hero";
-import Marquee from "./component/marquee";
+// import Marquee from "./component/marquee";
 import ProjectSection from "./component/ProjectSection";
 import SkillGrid from "./component/skills";
 
 function App() {
-  // useEffect(() => {
-  //   const setVH = () => {
-  //     const vh = window.innerHeight * 0.01;
-  //     document.documentElement.style.setProperty("--vh", `${vh}px`);
-  //   };
-
-  //   setVH();
-
-  //   window.addEventListener("resize", setVH);
-  //   return () => window.removeEventListener("resize", setVH);
-  // }, []);
-
   return (
     <>
       <Header />
